5302fbc15d9ce4f722caaab75a9da05d4bdf7665
Viacheslav Okolitiy <okolitiy.viacheslav@gmail.com>
2012-02-08 23:00:08 +0200

    One

--- src/eu/dimalex/afisha/provider/NewsDatabase.java
+++ src/eu/dimalex/afisha/provider/NewsDatabase.java
@@ -1,5 +1,76 @@
 package eu.dimalex.afisha.provider;
 
-public class NewsDatabase {
+import eu.dimalex.afisha.provider.NewsContract.Events;
+import eu.dimalex.afisha.provider.NewsContract.Favourites;
+import eu.dimalex.afisha.provider.NewsContract.Places;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
 
+public class NewsDatabase extends SQLiteOpenHelper{
+
+	private static final String DATABASE_NAME = "news.db";
+	private static final int DATABASE_VERSION = 1;
+	
+	public NewsDatabase(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	} 
+	
+	public NewsDatabase(Context context){
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	
+	public interface ITables {
+		String EVENTS = "events";
+		String PLACES = "places";
+		String FAVOURITES = "favourites";
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+//		db.execSQL("create table " + INewsDatabaseTables.EVENTS + "(_id integer primary key autoincrement," +
+//				"eventName text not null, date text not null, unique("+Events.ID+") ON CONFLICT REPLACE");
+//		db.execSQL("create table " + INewsDatabaseTables.PLACES + "(_id integer primary key autoincrement," +
+//				"name text not null,eventName text not null, unique("+Places.ID+") ON CONFLICT REPLACE");
+		db.execSQL(CREATE_TABLE_EVENTS);
+		db.execSQL(CREATE_TABLE_PLACES);
+		db.execSQL(CREATE_TABLE_FAVOURITES);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("DROP TABLE IF EXISTS " + ITables.EVENTS);
+		db.execSQL("DROP TABLE IF EXISTS " + ITables.PLACES);
+		db.execSQL("DROP TABLE IF EXISTS " + ITables.FAVOURITES);
+		onCreate(db);
+	}
+	
+	private static final String CREATE_TABLE_EVENTS = "CREATE TABLE "
+            + ITables.EVENTS + " ("
+            + Events._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + Events.EVENT_ID + " INTEGER, "
+            + Events.EVENT_DESCRIPTION + " TEXT, "
+            + Events.EVENT_DATE + " TEXT, "
+            + Events.EVENT_PLACE + " TEXT, "
+            + "UNIQUE (" + Events.EVENT_ID + ") ON CONFLICT REPLACE)";
+	
+	private static final String CREATE_TABLE_PLACES = "CREATE TABLE "
+            + ITables.PLACES + " ("
+            + Places._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + Places.PLACE_ID + " INTEGER, "
+            + Places.PLACE_ADDRESS + " TEXT, "
+            + "UNIQUE (" + Places.PLACE_ID + ") ON CONFLICT REPLACE)";
+	
+	private static final String CREATE_TABLE_FAVOURITES = "CREATE TABLE "
+            + ITables.FAVOURITES + " ("
+            + Favourites._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + Favourites.FAVOURITE_ID + "INTEGER,"
+            + Favourites.EVENT_NAME + " TEXT, "
+            + Favourites.PLACE_NAME + " TEXT, "
+            + Favourites.PLACE_ADDRESS + " TEXT, "
+            + Favourites.EVENT_DESCRIPTION + " TEXT, "
+            + "UNIQUE (" + Favourites.FAVOURITE_ID + ") ON CONFLICT REPLACE)";
 }
--- src/eu/dimalex/afisha/provider/NewsProvider.java
+++ src/eu/dimalex/afisha/provider/NewsProvider.java
@@ -1,5 +1,99 @@
 package eu.dimalex.afisha.provider;
 
-public class NewsProvider {
+import eu.dimalex.afisha.provider.NewsContract.Events;
+import eu.dimalex.afisha.provider.NewsContract.Favourites;
+import eu.dimalex.afisha.provider.NewsContract.Places;
+import eu.dimalex.afisha.provider.NewsDatabase.ITables;
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+public class NewsProvider extends ContentProvider{
+
+	private static final UriMatcher sUriMatcher = buildUriMatcher();
+	
+	private static final int EVENTS = 50;
+	private static final int PLACES = 100;
+	private static final int FAVOURITES = 150;
+	
+	private NewsDatabase mDatabase;
+	
+	private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = NewsContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, "events", EVENTS);
+        matcher.addURI(authority, "places", PLACES);
+        matcher.addURI(authority, "favourites", FAVOURITES);
+        
+        return matcher;
+    }
+	
+	protected String matchTable(Uri uri){
+		final int match = sUriMatcher.match(uri);
+		switch(match){
+		case EVENTS:
+			return ITables.EVENTS;
+		case PLACES:
+			return ITables.PLACES;
+		case FAVOURITES:
+			return ITables.FAVOURITES;
+		default:
+			throw new UnsupportedOperationException("Table doesn't match : " + uri);
+		}
+	}
+	
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		final SQLiteDatabase db = mDatabase.getWritableDatabase();
+		final String table = matchTable(uri);
+		int retVal = db.delete(table, selection, selectionArgs);
+        getContext().getContentResolver().notifyChange(uri, null);
+        return retVal;
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		final int match = sUriMatcher.match(uri);
+		switch(match){
+		case EVENTS:
+			return Events.CONTENT_TYPE;
+		case PLACES:
+			return Places.CONTENT_TYPE;
+		case FAVOURITES:
+			return Favourites.CONTENT_TYPE;
+		default:
+			throw new UnsupportedOperationException("Bad Uri: " + uri);
+		}
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean onCreate() {
+		mDatabase = new NewsDatabase(getContext());
+		return true;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
